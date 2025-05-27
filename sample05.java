c70662cc8636556ebd6a98e1a3526d2b649cf357
中出 恵美 <me-nakade@secom.co.jp>
2025-05-27 16:01:19 +0900

    fix_conflict month-day

--- sample05.java
+++ sample05.java
@@ -1,12 +1,6 @@
 import java.io.*;
 public class sample05{
     public static void main(String[] args)throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader( System.in ) );
-        System.out.print("数字を入力してください(1~12)：");
-        int month = Integer.parseInt(br.readLine());
-
-        //入力された月の日数を表示
-
         //入力された月の日本名を表示
         monthJapan(month);
     }
